32010dc9fba98f24f6fe21c6d0a47b4d0967a1b0
Brayden Phillips <brayden@Braydens-MacBook-Pro-2.local>
2022-12-05 22:28:19 +1000

    wip

--- stacks/feature/src/feature/eval-feature/load-test.k6.js
+++ stacks/feature/src/feature/eval-feature/load-test.k6.js
@@ -3,15 +3,15 @@ import { check, sleep } from 'k6';
 
 export const options = {
     stages: [
-        { duration: '1m', target: 300 }, // below normal load
-        // { duration: '5m', target: 100 },
-        // { duration: '2m', target: 200 }, // normal load
-        // { duration: '5m', target: 200 },
-        // { duration: '2m', target: 300 }, // around the breaking point
-        // { duration: '5m', target: 300 },
-        // { duration: '2m', target: 400 }, // beyond the breaking point
-        // { duration: '5m', target: 400 },
-        // { duration: '10m', target: 0 }, // scale down. Recovery stage.
+        { duration: '2m', target: 10 }, // below normal load
+        { duration: '5m', target: 100 },
+        { duration: '2m', target: 200 }, // normal load
+        { duration: '5m', target: 200 },
+        { duration: '2m', target: 300 }, // around the breaking point
+        { duration: '5m', target: 300 },
+        { duration: '2m', target: 400 }, // beyond the breaking point
+        { duration: '5m', target: 400 },
+        { duration: '10m', target: 0 }, // scale down. Recovery stage.
     ],
     thresholds: {
         // the rate of successful checks should be higher than 95%
